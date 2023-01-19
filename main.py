b05a218ea003e1ac357a75641ae3151316a79f9a
Myrela Bauman <121203121+MyreLab@users.noreply.github.com>
2023-01-19 16:38:20 -0500

    added comments, changed length, changed list characters

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+#importing modules
 import string as s
 import secrets
 
-
+#creating password function
 def generate_password(length: int, symbols: bool, uppercase: bool):
+
+    #combination of lowercase letters and numbers
     combination = s.ascii_lowercase + s.digits
 
+    #use punctuation string for symbols
     if symbols:
         combination += s.punctuation
 
+    #use uppercase string for uppercase characters
     if uppercase:
         combination += s.ascii_uppercase
 
     combination_length = len(combination)
 
+    #blank string will be populated by new password
     new_password = ""
 
     for _ in range(length):
@@ -20,7 +26,9 @@ def generate_password(length: int, symbols: bool, uppercase: bool):
 
     return new_password
 
-
+#Printing password
+#Change length, symbols, and uppercase as needed
+#Enumerate function creates 5 different passwords in a list form
 for _, index in enumerate(range(5)):
-    password = generate_password(length=20, symbols=True, uppercase=True)
-    print(index + 1, ">>", password)
+    password = generate_password(length=15, symbols=True, uppercase=True)
+    print(index + 1, ":", password)
